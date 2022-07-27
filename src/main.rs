1d241e260c91790ceaea8c9c2a07fa8fbd0df350
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2022-07-27 23:28:50 +0200

    print-tree: new feature/subcommand for printing current i3 layout

--- src/main.rs
+++ src/main.rs
@@ -25,9 +25,12 @@ use crate::command_executor::CommandExecutor;
 use crate::event_listener::EventListener;
 use crate::event_listener::EventSubscribe;
 use crate::tabmode::TabMode;
+use anyhow::anyhow;
 use anyhow::Context;
 use anyhow::Result;
 use clap::Parser;
+use print_tree::print_tree;
+use utilities::find_node_by_id;
 
 /// CLI arguments.
 #[derive(clap::Parser)]
@@ -41,17 +44,28 @@ struct CliArgs {
 /// Subcommand of CLI.
 #[derive(clap::Subcommand)]
 enum Command {
-    /// Autolayout service.
+    /// Run autolayout service.
     #[clap(name = "autolayout")]
     Autolayout,
 
-    /// TabMode executor.
+    /// Toggle tabmode on the current focused workspace.
     #[clap(name = "tabmode")]
     TabMode,
 
     /// Display i3 information.
     #[clap(name = "i3version")]
     I3Version,
+
+    /// Print a snapshot of the current layout as tree.
+    #[clap(name = "print-tree")]
+    PrintTree(PrintTreeCmd),
+}
+
+/// Information about the print-tree command.
+#[derive(clap::Args)]
+struct PrintTreeCmd {
+    /// The workspace number to print of. If not specified prints all workspaces.
+    workspace_num: Option<i32>,
 }
 
 fn main() -> Result<()> {
@@ -61,6 +75,9 @@ fn main() -> Result<()> {
         Command::Autolayout => command_autolayout().context("Failure in command 'autolayout'"),
         Command::TabMode => command_tabmode().context("Failure in command 'tabmode'"),
         Command::I3Version => command_i3_version().context("Failure in command 'i3version'"),
+        Command::PrintTree(print_tree_cmd) => {
+            command_print_tree(print_tree_cmd).context("Failure in command 'print-tree'")
+        }
     }
 }
 
@@ -95,8 +112,31 @@ fn command_i3_version() -> Result<()> {
     Ok(())
 }
 
+/// Print the snapshot of I3 layout in the tree fashion.
+fn command_print_tree(print_tree_cmd: PrintTreeCmd) -> Result<()> {
+    let mut command_executor = CommandExecutor::new()?;
+    let root_node = command_executor.query_root_node()?;
+
+    let node = match print_tree_cmd.workspace_num {
+        Some(workspace_num) => {
+            let workspace = command_executor
+                .query_workspaces()?
+                .into_iter()
+                .find(|workspace| workspace.num == workspace_num)
+                .ok_or_else(|| anyhow!("Cannot find the workspace number '{}'", workspace_num))?;
+
+            find_node_by_id(workspace.id, &root_node)
+                .context("Cannot find the workspace associated with the id")?
+        }
+        None => root_node.node(),
+    };
+
+    print_tree(node)
+}
+
 mod autolayout;
 mod command_executor;
 mod event_listener;
+mod print_tree;
 mod tabmode;
 mod utilities;
---
+++ src/print_tree.rs
@@ -0,0 +1,57 @@
+/*
+    Copyright (C) 2022  Biagio Festa
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+use crate::command_executor::I3Node;
+use anyhow::Context;
+use anyhow::Result;
+use ptree::TreeItem;
+use std::borrow::Cow;
+use std::io::Write;
+
+/// Print the tree associated starting from a root node.
+pub fn print_tree(node: &I3Node) -> Result<()> {
+    ptree::print_tree(&TreeNode(node)).context("Cannot print i3 tree")
+}
+
+#[derive(Clone)]
+struct TreeNode<'a>(&'a I3Node);
+
+impl<'a> TreeItem for TreeNode<'a> {
+    type Child = TreeNode<'a>;
+
+    fn write_self<W>(&self, f: &mut W, _style: &ptree::Style) -> std::io::Result<()>
+    where
+        W: Write,
+    {
+        write!(f,
+               "[Type: {type:?}; \
+                Name: {name:?}; \
+                Layout: {layout:?}; \
+                WinType: {wintype:?}; \
+                NumFloatings: {num_floats}]",
+               type = self.0.node_type,
+               name = self.0.name,
+               layout = self.0.layout,
+               wintype = self.0.window_type,
+               num_floats = self.0.floating_nodes.len(),
+        )
+    }
+
+    fn children(&self) -> Cow<[Self::Child]> {
+        Cow::from(self.0.nodes.iter().map(TreeNode).collect::<Vec<_>>())
+    }
+}
